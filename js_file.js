39c6c58532c006da1c906b8e41329f2366e985d5
Harshvardhan561 <harshvardhanarra@gmail.com>
2022-02-09 14:24:14 +0530

    Js second change

--- js_file.js
+++ js_file.js
@@ -1,4 +1,4 @@
-var a;
+/*var a;
 a=10
 console.log(typeof a);
 console.log(a);
@@ -14,4 +14,22 @@ console.log(typeof a);
 
 a=true
 console.log(a);
-console.log(typeof a);
+console.log(typeof a);*/
+
+var num =10;
+console.log(num);
+
+var float = 2.4;
+console.log(float);
+
+var t=ture;
+var f =false;
+console.log(t);
+console.log(f);
+
+//String :'' ,"" , `` . 
+var str ='a';
+console.log(typeof str);
+
+str = "hello how are ya!";
+console.log(str);
