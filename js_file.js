ba1e6c5b79f39a90767dc1645310e8cd28154032
Harshvardhan561 <harshvardhanarra@gmail.com>
2022-02-09 13:37:25 +0530

    Started JS

---
+++ js_file.js
@@ -0,0 +1,17 @@
+var a;
+a=10
+console.log(typeof a);
+console.log(a);
+
+
+a="hello"
+console.log(a);
+console.log(typeof a);
+
+a=null
+console.log(a);
+console.log(typeof a);
+
+a=true
+console.log(a);
+console.log(typeof a);
